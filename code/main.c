cc3f136dc942e864b767d4c0abac1d0e47f62d7c
309631 <85329098+309631@users.noreply.github.com>
2021-11-14 20:14:07 +0100

    Update main.c
    adding the communication

--- code/main.c
+++ code/main.c
@@ -1,33 +1,41 @@
-#include <dht.h>  // Include library
+#include <DHT.h>
+#include <DHT_U.h>
+
 #include <LiquidCrystal_I2C.h>
-#define TEMP_SENSOR_PORT 8  // Defines pin number to which the sensor is connected
+
+#define TEMP_SENSOR_PIN 8  // Defines pin number to which the sensor is connected
 #define LED_TEMP_PORT 2
+#define PHOTORESISTOR_PORT A0
+#define DHTTYPE DHT11
+
 #define MAX_HUMIDITY 50
 #define MAX_TEMP 24
 
+#define MEASURE_DELAY_MINUTES .1
+
+//DEFINES FOR COMMUNICATION CODES
+#define GET_DATA 1
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+DHT dht = DHT(TEMP_SENSOR_PIN, DHTTYPE);      // Creates a DHT object
 
-//void check(int value, int value_max, int PORT)
-//{
-//  if(value>value_max)
-//  {digitalWrite(PORT,HIGH);}
-//  else
-//  {digitalWrite(PORT,LOW);}
-//}
-
-dht DHT;      // Creates a DHT object
+void communication();
 
 void setup() {
   Serial.begin(9600);
   lcd.init(); // initialize the lcd
   lcd.backlight();
+  pinMode(PHOTORESISTOR_PORT, INPUT);
+  Serial.println("time,temperature,humidity,light");
+
+  dht.begin();
 }
 
 void loop() {
-  int readData = DHT.read11(TEMP_SENSOR_PORT);
-  float t = DHT.temperature;  // Read temperature
-  float h = DHT.humidity;   // Read humidity
+  float t = dht.readTemperature();  // Read temperature
+  float h = dht.readHumidity();   // Read humidity
+  int light = analogRead(A0);
 //  check(h,MAX_HUMIDITY,LED_TEMP_PORT);
 //  check(t,MAX_TEMP,LED_TEMP_PORT);
 
@@ -36,26 +44,43 @@ if(h>MAX_HUMIDITY || t>MAX_TEMP)
   else
   {digitalWrite(LED_TEMP_PORT,LOW);}
 
+//  function for receiving data through Serial Port/UART
+  communication();
 
-//  Serial.print("Temperature = ");
-//  Serial.print(t);
-//  Serial.print("°C | ");
-//  Serial.print((t*9.0)/5.0+32.0); // Convert celsius to fahrenheit
-//  Serial.println("°F ");
-//  Serial.print("Humidity = ");
-//  Serial.print(h);
-//  Serial.println("% ");
-//  Serial.println("");
 
   
   lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("TEMP = ");
+  lcd.print("T= ");
   lcd.print(t); 
-  lcd.print("°C");
+  lcd.print("st C");
   lcd.setCursor(2, 1);         // move cursor to   (2, 1)
-  lcd.print("HUM = "); // print message at (2, 1)
+  lcd.print("H="); // print message at (2, 1)
   lcd.print(h); // print message at (2, 1)
-  lcd.print("%"); // print message at (2, 1)
+  lcd.print("% "); // print message at (2, 1)
+  lcd.print("L=");
+  lcd.print(light); 
 
-  delay(2000); // wait two seconds
+  delay(MEASURE_DELAY_MINUTES*60000); // wait two seconds
+}
+
+
+
+void communication(){ 
+  
+  if(Serial.available() != 0)
+  {
+  int COM = 0;
+  COM = Serial.read();
+
+  switch(COM)
+  {
+
+    case GET_DATA:
+      Serial.print(dht.readTemperature());
+      Serial.print(",");
+      Serial.print(dht.readHumidity());
+      Serial.print(",");
+      Serial.println(analogRead(A0));
+  }
+  }
 }
